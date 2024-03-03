b6e5b8a3a438bd9d91ce778e74e8a4431dafd6c0
Fares Masmoudi <faresmasmoudi@outlook.com>
2024-03-03 14:31:12 +0100

    initialized spring boot project

---
+++ src/main/java/tn/iit/labmanager/LabmanagerApplication.java
@@ -0,0 +1,13 @@
+package tn.iit.labmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabmanagerApplication.class, args);
+	}
+
+}
